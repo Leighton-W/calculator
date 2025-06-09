cd28e08f65a07fb2e515639d9ceda39e72e26b21
Leighton Welby <lwelby27@gmail.com>
2025-06-09 17:55:21 +0100

    Created Input Prompt
    created a prompt that will store the users choice in a variable

--- javascript.js
+++ javascript.js
@@ -1,26 +1,24 @@
 console.log("Hello World!")
 
-
 function add(a,b){
     return a + b;
 }
-
-console.log(add(2,3))
 
 function subtract(a,b){
     return a - b;
 }
 
-console.log(subtract(5,3))
-
 function multiply(a,b){
     return a * b;
 }
-
-console.log(multiply(2,3))
 
 function divide(a,b){
     return a / b;
 }
 
-console.log(divide(6,2))
+
+const input1 = prompt("Please choose a number")
+const input2 = prompt("Please choose a number")
+
+console.log(input1)
+console.log(input2)
