b8f89c80a628f795ec650389c488dc5db9ec1d64
Leighton Welby <lwelby27@gmail.com>
2025-06-09 18:04:26 +0100

    Created Operation Function
    created a function that takes the users inputs and applies the correcting operators function

--- javascript.js
+++ javascript.js
@@ -17,8 +17,25 @@ function divide(a,b){
 }
 
 
-const input1 = prompt("Please choose a number")
-const input2 = prompt("Please choose a number")
+const input1 = prompt("Please choose a number");
+const input2 = prompt("Please choose a number");
+const operator = prompt("Please select an operator(+,-,*,/)");
 
 console.log(input1)
 console.log(input2)
+console.log(operator)
+
+
+function operation(input1, input2, operator){
+    if (operator == "+"){
+        return input1 + input2;
+    }else if (operator == "-"){
+        return input1 - input2;
+    }else if (operator == "*"){
+        return input1 * input2;
+    }else if (operator == "/"){
+        return input1 / input2;
+    }
+}
+
+console.log(operation(input1,input2,operator));
